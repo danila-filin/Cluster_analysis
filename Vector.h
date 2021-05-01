d36230d6189afe641ca6e292c09a00dedc1790e1
danila-filin <78472993+danila-filin@users.noreply.github.com>
2021-05-01 13:36:29 +0300

    Add files via upload

--- Vector.h
+++ Vector.h
@@ -13,32 +13,24 @@ using namespace std;
 class Vector
 {
 private:
-    double first_coordinate, second_coordinate, function_value;
+    double first_coordinate, second_coordinate, function_value; 
     int vector_id;
 public:
-    //конструкторы и деструкторы
     Vector();
     Vector(double x, double y);
     Vector(const Vector& v);
     ~Vector() = default;
-
-    const Vector& operator=(const Vector& v);//операция присваивания
-
-    //операции с векторами
+    const Vector& operator=(const Vector& v);
     friend Vector operator+(const Vector& v, const Vector& u);
     friend Vector operator-(const Vector& v, const Vector& u);
     friend Vector operator*(double lambda, const Vector& v);
     friend Vector operator*(const Vector& v, double lambda);
-
-    double length();//нахождение длины вектора
-
-    //функции, связанные с атрибутами данного класса
+    double length();
     double get_first_coordinate();
     double get_second_coordinate();
     double get_function_value();
     int get_vector_id();
     void assign_vector_id(int id);
-
-    void print_vector(ofstream& file);//печать вектора в файл
+    void print_vector(ofstream& file);
 };
 
